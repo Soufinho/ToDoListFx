d356f917bf3a2c5d1b8102ba9a6e634a0c3d1fac
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-03-18 12:29:20 +0100

    Création d'un modele et d'un repository Utilisateur et ajout d'une méthode pour insérer un utilisateur

---
+++ src/main/java/Repository/UtilisateurRep.java
@@ -0,0 +1,72 @@
+package Repository;
+
+
+import model.Utilisateur;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static database.Database.getConnexion;
+
+
+public class UtilisateurRep {
+    private Connection connexion;
+
+    public UtilisateurRep(Connection connexion) {
+        this.connexion= getConnexion();
+    }
+
+    public void ajouterUtilisateur(Utilisateur utilisateur) {
+        String sql = "INSERT INTO utilisateurs (nom, prenom, email, mdp, role) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement stmt = connexion.prepareStatement(sql);
+            stmt.setString(1, utilisateur.getNom());
+            stmt.setString(2, utilisateur.getPrenom());
+            stmt.setString(3, utilisateur.getEmail());
+            stmt.setString(4, utilisateur.getMdp());
+            stmt.setString(5, utilisateur.getRole());
+            stmt.executeUpdate();
+            System.out.println("Utilisateur ajouté avec succès !");
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de l'ajout de l'utilisateur : " + e.getMessage());
+        }
+    }
+
+    public Utilisateur getUtilisateurParEmail(String email) {
+        String sql = "SELECT * FROM utilisateurs WHERE email = ?";
+        try {
+            PreparedStatement stmt = connexion.prepareStatement(sql);
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()) {
+                new Utilisateur();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    public ArrayList<Utilisateur> getTousLesUtilisateurs() {
+        String sql = "SELECT * FROM utilisateurs";
+        ArrayList<Utilisateur> utilisateurs = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connexion.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                Utilisateur u = new Utilisateur();
+                utilisateurs.add(u);
+            }
+            return utilisateurs;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return utilisateurs;
+    }
+
+}
--- src/main/java/model/Utilisateur.java
+++ src/main/java/model/Utilisateur.java
@@ -3,22 +3,51 @@ package model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Utilisateur extends Application {
+public abstract class Utilisateur extends Application {
     private String nom;
     private String prenom;
     private String email;
     private String mdp;
+    private int id;
+    private String role;
 
     public static void main(String[] args) {
 
         launch(args);
     }
 
-public Utilisateur(String nom, String prenom, String email, String mdp) {
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", mdp='" + mdp + '\'' +
+                ", id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    public Utilisateur(String nom, String prenom, String email, String mdp, int id, String role) {
         this.nom = nom;
         this.prenom = prenom;
         this.email = email;
         this.mdp = mdp;
+        this.id = id;
+        this.role = role;
+}
+
+public Utilisateur(String nom, String prenom, String email, String mdp, String role) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+        this.role = role;
+}
+
+public Utilisateur(String mdp, String email) {
+        this.mdp = mdp;
+        this.email = email;
 }
 
 
@@ -54,8 +83,19 @@ public Utilisateur(String nom, String prenom, String email, String mdp) {
         this.mdp = mdp;
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
